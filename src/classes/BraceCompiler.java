547a23654d1501ae40b6751c4594d4eccd023e95
Vellmond <edgar.bautista@edu.uag.mx>
2023-06-13 12:54:28 -0600

    update change

--- src/classes/BraceCompiler.java
+++ src/classes/BraceCompiler.java
@@ -1,47 +1,53 @@
 package classes;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class BraceCompiler {
-        public static List<String> braces(String[] values) {
-            List<String> result = new ArrayList<>();
+    public static List<String> braces(String[] values) {
+        List<String> result = new ArrayList<>();
 
-            for (String value : values) {
-                if (isBalanced(value)) {
-                    result.add("YES");
-                } else {
-                    result.add("NO");
+        for (String value : values) {
+            if (isBalanced(value)) {
+                result.add("YES");
+            } else {
+                result.add("NO");
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isBalanced(String value) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : value.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty() || !isMatchingPair(stack.pop(), c)) {
+                    return false;
                 }
             }
-
-            return result;
         }
 
-        private static boolean isBalanced(String value) {
-            Deque<Character> stack = new ArrayDeque<>();
+        return stack.isEmpty();
+    }
 
-            for (char c : value.toCharArray()) {
-                if (c == '(' || c == '{' || c == '[') {
-                    stack.push(c);
-                } else if (c == ')' || c == '}' || c == ']') {
-                    if (stack.isEmpty() || !isMatchingPair(stack.pop(), c)) {
-                        return false;
-                    }
-                }
-            }
+    private static boolean isMatchingPair(char opening, char closing) {
+        return (opening == '(' && closing == ')') ||
+                (opening == '{' && closing == '}') ||
+                (opening == '[' && closing == ']');
+    }
 
-            return stack.isEmpty();
+    public static void main(String[] args) {
+        String[] values = {"{}[]", "()", "{[]}"};
+        List<String> result = braces(values);
+        for (String str : result) {
+            System.out.println(str);
         }
-
-        private static boolean isMatchingPair(char opening, char closing) {
-            return (opening == '(' && closing == ')') ||
-                    (opening == '{' && closing == '}') ||
-                    (opening == '[' && closing == ']');
-        }
-
-        public static void main(String[] args) {
-            String[] values = {"{}[]", "()", "{[}]}"};
-            List<String> result = braces(values);
-            for (String str : result) {
-                System.out.println(str);
-            }
-        }
+    }
 }
+
